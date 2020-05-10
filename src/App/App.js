192e9a763ce0bfe52e45d22d69e375dc816a3bab
Sarah Holder <sr.holder@yahoo.com>
2020-05-09 22:38:49 -0500

    setup

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1>SPORTS ROSTER</h1>
         <button className="btn btn-dark">Button</button>
       </div>
